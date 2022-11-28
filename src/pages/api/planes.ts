bdc0e5ffeae0d05e85d5b5f2d759e69eef9f711a
Joe Ciesielski <jtcies@gmail.com>
2022-11-28 12:57:21 -0500

    adding params to api

--- src/pages/api/planes.ts
+++ src/pages/api/planes.ts
@@ -1,10 +1,35 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Plane, QueryParams } from "../../interfaces";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-      const resData = await fetch(
-        "https://opensky-network.org/api/states/all?lamin=45.8389&lomin=5.9962&lamax=47.8229&lomax=10.5226"
-      )
-      .then((response) => response.json())
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Plane[]>) {
+    const query: QueryParams = req.query
+    // const lat: number = Number(query.lat ?? Math.random().toString())
+    // const lon: number = Number(query.lat ?? Math.random().toString())
+    const lat: number = Number(query.lat ?? process.env.DEFAULT_LAT)
+    const lon: number = Number(query.lon ?? process.env.DEFAULT_LON)
+    const range = Number(query.range ?? process.env.DEFAULT_RANGE)
 
-      return res.status(200).json({ data: resData.states });
+    const ymin = 'lamin=' + (lat - range).toString()
+    const ymax = 'lamax=' + (lat + range).toString()
+    const xmin = 'lomin=' + (lon - range).toString()
+    const xmax = 'lomax=' + (lon + range).toString()
+
+    const queryString = [ymin, ymax, xmin, xmax].join('&')
+
+    const resData = await fetch(
+        "https://opensky-network.org/api/states/all?" + queryString, {
+        headers: {
+            "Content-Type": "text/plain",
+            'Authorization': 'Basic ' + btoa(process.env.OPENSKY_USER?.toString() + ':' + process.env.OPENSKY_PASSWORD?.toString()),
+            }
+        }
+    )
+    .then((response) => response.json())
+    .then((data) => data.states)
+    .then((data: Plane[]) => {
+    data.sort((a: Plane, b: Plane) => b.geo_altitude - a.geo_altitude)
+    return data
+    })
+
+    return res.status(200).json( resData );
   }
