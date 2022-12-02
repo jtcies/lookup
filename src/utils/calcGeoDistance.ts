05cb89c61014e769d7579ff91a620368502b8aa6
Joe Ciesielski <jtcies@gmail.com>
2022-12-02 16:01:00 -0500

    using const instead of let

--- src/utils/calcGeoDistance.ts
+++ src/utils/calcGeoDistance.ts
@@ -14,18 +14,18 @@ export default function geoDistance(
     lat2 = lat2 * Math.PI / 180;
 
     // Haversine formula
-    let dlon = lon2 - lon1;
-    let dlat = lat2 - lat1;
-    let a = Math.pow(Math.sin(dlat / 2), 2)
+    const dlon = lon2 - lon1;
+    const dlat = lat2 - lat1;
+    const a = Math.pow(Math.sin(dlat / 2), 2)
         + Math.cos(lat1) * Math.cos(lat2)
         * Math.pow(Math.sin(dlon / 2),2);
 
-    let c = 2 * Math.asin(Math.sqrt(a));
+        const c = 2 * Math.asin(Math.sqrt(a));
 
     // Radius of earth in kilometers. 
-    let r = 6371000;
+    const r = 6371000;
 
     // calculate the result
-    let groundDist = c * r;
+    const groundDist = c * r;
     return groundDist
 }
